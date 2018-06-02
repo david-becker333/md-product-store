a8a4717443d08f9fe6a9cc9a5a87e2dfe13fff63
David Becker <davidbecker@gmail.com>
2018-06-02 12:05:16 -0700

    Updated README and detail page.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # md-product-store
-A React based product store with a material design theme.
+A React based product store with a material design theme.  This project is a work in progress but will eventually provide the essential features for a store catalog.
+
+The goal is to adhere to the material design theme standards.  It utilizes a bootstrap 4..1.x base with a bootstrap material design library that alters the standard boostrap into a material design look and feel.
+
+The future directon for state management will be based on react redux saga for state management.
+
+__Table of content__
+
+- [Install](#install)
+- [Usage examples](#usage-examples)
+
+## Install
+
+**node.js**:
+
+Once downloaded run the following commands to install and start the application.  The application will open webpack-dev-server and listen on port 5000.  This can be changed to meet your needs.
+
+```bash
+npm install
+
+npm start
+```
+
+
+## Usage examples
+
+See also: In progress
--- src/components/view/product/product-detail.js
+++ src/components/view/product/product-detail.js
@@ -7,52 +7,7 @@ export default class ProductDetail extends React.Component {
      }
 
     render() {
-        return (<h1>Category</h1>);
+        return (<h1>TODO: Product Catalog</h1>);
     }
     
 }
-
-
-
-
-
-/**
-     
-    handleChange = (e) => {
-        console.log(this)
-        e.preventDefault();
-        const { name, value } = e.target;
-        if (name) {
-            this.setState({
-                [name]: value
-            })
-        }
-    }
-
-    validate = (e) => {
-        e.preventDefault();
-    }
-        //this.handleChange = this.handleChange.bind(this);
-       // console.log("ProductCatalog: ", this.state.products)
-    <br/>
-                <form name="userInfoForm">
-                    <div className="form-group">
-                        <label htmlFor="email">Email address
-                        <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
-                        </label>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="phone">Primary phone 
-                        <input type="phone" name="phone" value={this.state.phone} onChange={this.handleChange} />
-                        </label>
-                    </div>
-                    <button type="submit" className="btn btn-primary" onClick={this.validate}>Submit</button>
-                </form>
-
-
-                 <ul>
-                {items && items.map((product, i) => <li key={i}>{product.name}</li>)}
-            </ul>
- */
-
-
