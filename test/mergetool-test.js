7c0d24896835c43fc4599f4b412b5dce546f4897
David Becker <davidbecker@gmail.com>
2018-07-21 17:19:27 -0700

    adding simple code example to try merge tools

---
+++ test/mergetool-test.js
@@ -0,0 +1,17 @@
+// some sample code to test different mergetools available
+
+function add(a, b) {
+    pass
+}
+
+function subtract(a, b) {
+    pass
+}
+
+function multiply(a, b) {
+    pass
+}
+
+function divide(a, b) {
+    pass
+}
